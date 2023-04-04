69d51bad31f75d708171aa085cadbbcc8414ba47
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-04-04 23:21:58 +0900

    微調整

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
-const dbPath = "app/database.sqlite3";
+const dbPath = '/Users/hamiltonshiro/Desktop/WebApiBasic/app/SQL/database.sqlite3';
 
 //Get all users
-app.get('app/v1/users', (req,res) => {
+app.get('api/v1/users', (req,res) => {
     const db = new sqlite3.Database(dbPath);    //データベースに接続
 
     db.all('SELECT * FROM users', (err, rows) => {
         res.json(rows)
     })
-    db.close()
+    db.close()  //データベース接続終了
 })
-
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log("Listen on port:"+port)
