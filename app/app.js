660890fc09704b231d687c4ba25e2f2efe8d9eb6
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-06-03 21:46:12 +0900

    MySQLの接続とレスポンス完了

--- app/app.js
+++ app/app.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const app = express();
-const sqlite3 = require("sqlite3");
-const dbPath = "users.db";
+const mysql = require("mysql2");
+// const sqlite3 = require("sqlite3");
+// const dbPath = "users.db";
+
+//app.use(express.static("public"));
+
+//mysqlの接続に必要な情報
+const connection = mysql.createConnection({
+  host: "db",
+  user: "shiro",
+  password: "shiro",
+  database: "api_basic"
+});
+
+//mysql接続
+// con.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connected");
+// });
 
 //Get all users
 app.get("/api/users", (req, res) => {
-  const db = new sqlite3.Database(dbPath);
-  db.all("select * from users", (err, rows) => {
-    res.json(rows);
+  connection.query("select * from users", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
   });
-  db.close();
-});
+  // res.json({ id: 1, name: "Yui" });
 
-const port = process.env.PORT || 3000;
+  // const db = new sqlite3.Database(dbPath);
+  // db.all("select * from users", (err, rows) => {
+});
+// db.close();
+// });
+
+const port = 3000;
 app.listen(port);
 console.log(port + "server opened!");
