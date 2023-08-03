83f8416b1def5459fcdfb3802b094ec67cf268e9
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-08-04 00:20:25 +0900

    ルーティングを使用してpostメソッドの実装

--- app/app.js
+++ app/app.js
@@ -1,53 +1,100 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const connection = require("./db");
-// const router = express.Router();
+// const connection = require("./db");
 
-// URL/user/...
-const userRouter = require("./router/user");
+// // URL/user/...
+// const userRouter = require("./router/user");
 
 // URL/api/....
 const apiRouter = require("./router/api");
 
 //静的ファイルのルートディレクトリを設定
 //app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "views")));
+//app.use(express.static(path.join(__dirname, "views")));
 
 //テンプレートエンジン(動的ファイル)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-//ルーティング
-app.use("/user", userRouter);
+// //ルーティング
+// app.use("/user", userRouter);
+// app.use("/api", apiRouter);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use("/api", apiRouter);
 
-app.get("/", (req, res) => {
-  connection.query("select * from users", (err, results) => {
-    if (err) {
-      return "エラーです。";
-    } else {
-      //取得したデータをejsテンプレートに渡してhtmlを生成
-      res.render("index", { data: results });
-    }
-  });
-});
+// app.get("/", (req, res) => {
+//   connection.query("select * from users", (err, results) => {
+//     if (err) {
+//       return "エラーです。";
+//     } else {
+//       //取得したデータをejsテンプレートに渡してhtmlを生成
+//       res.render("index", { data: results });
+//     }
+//   });
+// });
 
-app.get("/search", (req, res) => {
-  const query = req.query.name;
-  console.log(query);
-  if (query) {
-    connection.query(
-      `select * from users where name like '%${query}%'`,
-      (err, results) => {
-        if (err) throw err;
-        console.debug(results);
-        res.render("search", { results, query });
-      }
-    );
-  } else {
-    res.render("search", { results: [], query: "" });
-  }
-});
+// app.get("/search", (req, res) => {
+//   const query = req.query.name;
+//   console.log(query);
+//   if (query) {
+//     connection.query(
+//       `select * from users where name like '%${query}%'`,
+//       (err, results) => {
+//         if (err) throw err;
+//         console.debug(results);
+//         res.render("search", { results, query });
+//       }
+//     );
+//   } else {
+//     res.render("search", { results: [], query: "" });
+//   }
+// });
 
-app.listen(3000);
+app.listen(3003);
+
+// const express = require("express");
+// const app = express();
+// const connection = require("./db");
+
+// // POSTリクエストのボディーをパースするためのミドルウェア
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// // ルートエンドポイント
+// app.get("/", (req, res) => {
+//   res.send("Hello, this is the home page!");
+// });
+
+// // 投稿フォームのルートエンドポイント
+// app.get("/form", (req, res) => {
+//   res.sendFile(__dirname + "/form.html");
+// });
+
+// // データベースからデータを取得して出力
+// app.get("/data", (req, res) => {
+//   connection.query("SELECT * FROM users", (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
+
+// // フォームデータを受け取ってデータベースに挿入
+// app.post("/submit", (req, res) => {
+//   const { name, profile } = req.body;
+//   const post = { name, profile };
+
+//   connection.query("INSERT INTO users SET ?", post, (err, result) => {
+//     if (err) throw err;
+//     res.send("Data inserted successfully!");
+//   });
+// });
+
+// // サーバーを起動
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
--- app/router/api.js
+++ app/router/api.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const connection = require("../db");
 
-//全ユーザー取得
-//@path api/users[GET]
-router.get("/users", (req, res) => {
-  connection.query("select * from users", (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.json(result);
-  });
-});
+//静的ファイルのルートディレクトリを設定
+//app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "views")));
+//app.use(express.static("views"));
+
+//テンプレートエンジン(動的ファイル)
+// router.set("view engine", "ejs");
+// router.set("views", path.join(__dirname, "../views"));
+
+//middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
 // app.get("/api/users", (req, res) => {
 //   connection.query("select * from users", (err, result) => {
 //     if (err) throw err;
@@ -28,25 +33,56 @@ router.get("/users", (req, res) => {
 // });
 
 //ユーザーの部分一致
-router.get("/search", (req, res) => {
-  const keyword = req.query.q;
-  connection.query(
-    `select * from users where name like '%${keyword}%'`,
-    (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.json(result);
-    }
-  );
-});
+// router.get("/search", (req, res) => {
+//   const keyword = req.query.q;
+//   connection.query(
+//     `select * from users where name like '%${keyword}%'`,
+//     (err, result) => {
+//       if (err) throw err;
+//       console.log(result);
+//       res.json(result);
+//     }
+//   );
+// });
 
 //idによるユーザー取得
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  connection.query(`select * from users where id=${id}`, (err, result) => {
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   connection.query(`select * from users where id=${id}`, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send(result);
+//   });
+// });
+
+router.use(express.static(path.join(__dirname, "../views")));
+
+router.get("/", (req, res) => {
+  res.send("Hello, this is the home page!");
+});
+
+router.get("/new", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/new.html"));
+});
+
+//全ユーザー取得
+//@path api/users[GET]
+router.get("/users", (req, res) => {
+  connection.query("select * from users", (err, result) => {
     if (err) throw err;
     console.log(result);
-    res.send(result);
+    res.render("index", { data: result }); //"index"はejsのファイル名
+  });
+});
+
+router.post("/create", (req, res) => {
+  const { name, profile } = req.body;
+  const post = { name, profile };
+
+  connection.query("INSERT INTO users SET ?", post, (err, result) => {
+    if (err) throw err;
+    console.log("Data inserted successfully!");
+    res.redirect("/api/users"); // Redirect after the data is inserted
   });
 });
 
